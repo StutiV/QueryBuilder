2e1bdb4596eae0816d1fb4c6a01252c9f9ab870b
Yellow_Ledbetter <stuti_v@protonmail.com>
2021-05-09 14:16:37 +0530

    initialization

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import QueryBuilder, { formatQuery, RuleGroupType } from "react-querybuilder";
+
+function App() {
+  const [query, setQuery] = useState<RuleGroupType>({
+    id: "root",
+    combinator: "and",
+    rules: [],
+  });
+
+  return (
+    <>
+      <QueryBuilder
+        fields={[{ name: "test", label: "Test" }]}
+        query={query}
+        onQueryChange={(q) => setQuery(q)}
+      />
+      <pre>{formatQuery(query, "sql")}</pre>
+      <pre>{formatQuery(query, "json")}</pre>
+    </>
+  );
+}
+
+export default App;
